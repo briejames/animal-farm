f00a04ca8181fb6a6dea71e14d015201c696accf
Brie James <briexjames@gmail.com>
2021-11-04 15:15:48 -0700

    added content to app.js

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 // import functions and grab DOM elements
+const dogSound = document.getElementById("dog-sound")
+const catSound = document.getElementById("cat-sound")
+const horseSound = document.getElementById("horse-sound")
 
+const dogImgEl = document.getElementById("dog-img")
+const catImgEl = document.getElementById("cat-img")
+const batImgEl = document.getElementById("bat-img")
+console.log
 // initialize global state
 
 // set event listeners 
